0bdb7451f87c1da3d324d2c7e0ac847d0cc63de5
Ashley Fajardo <a.e.fajardo06@gmail.com>
2025-11-14 09:32:54 -0600

    creacion de toString

--- src/progra2/s5/lab5/Game.java
+++ src/progra2/s5/lab5/Game.java
@@ -25,7 +25,27 @@ public class Game extends RentItem implements MenuActions {
         
     }
     
+    public Calendar getFechaPublicacion(){
+        return fechaPublicacion;
+    }
     
+    public void setFechaPublicacion(int anio, int mes, int dia){
+        
+        Calendar fechaNueva = Calendar.getInstance();
+        fechaNueva.set(anio, mes -1, dia);
+        
+        this.fechaPublicacion = fechaNueva;
+    }
+    
+    public String toString(){
+        
+        int anio = fechaPublicacion.get(Calendar.YEAR);
+        int mes = fechaPublicacion.get(Calendar.MONTH) + 1;
+        int dia = fechaPublicacion.get(Calendar.DAY_OF_MONTH);
+        
+        return super.toString() + ", Fecha publicacion: " + dia +
+                "/" + mes + "/" + anio + " - PS3 Game";
+    }
     
     
 }
