18dcf012334fd6a1daa2330372026cb661b6c0fb
Ashley Fajardo <a.e.fajardo06@gmail.com>
2025-11-14 09:19:41 -0600

    creacion de clase Game

---
+++ src/progra2/s5/lab5/Game.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package progra2.s5.lab5;
+
+/**
+ *
+ * @author ashley
+ */
+public class Game extends RentItem implements MenuActions {
+    
+}
